a5c95042c4bcfa38e5be6dc7ef321f15de365144
dfar45 <dharr244@montgomerycollege.edu>
2026-09-22 14:13:19 -0400

    Fix name initialization and loop condition

--- WriteToFileDemo1.java
+++ WriteToFileDemo1.java
@@ -7,10 +7,10 @@ import java.util.Scanner;
 public class WriteToFileDemo1 {
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner input = new Scanner(System.in);
-		String name;
+		String name="";
 		int age;
 		PrintWriter outputFile = new PrintWriter("names.txt");
-		while (!name.equals("end) {
+		while (!name.equals("end)) {
 			System.out.print("Enter your name: ");
 			name = input.next();
 			System.out.print("Enter your age: ");
